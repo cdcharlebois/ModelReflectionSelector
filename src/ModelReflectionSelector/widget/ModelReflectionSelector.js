4380a300b9ad9fb90604a6ee97b1f0ba0b51ab0a
Conner Charlebois <cdcharlebois@gmail.com>
2017-04-24 16:02:56 -0400

    initial. works at one level of depth

--- src/ModelReflectionSelector/widget/ModelReflectionSelector.js
+++ src/ModelReflectionSelector/widget/ModelReflectionSelector.js
@@ -15,10 +15,13 @@ define([
     "dojo/html",
     "dojo/_base/event",
     "ModelReflectionSelector/widget/lib/jquery-1.11.2",
+    "ModelReflectionSelector/widget/lib/jstree",
 
     "dojo/text!ModelReflectionSelector/widget/template/ModelReflectionSelector.html"
-], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, jQuery, widgetTemplate) {
+], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, jQuery, jsTree, widgetTemplate) {
     "use strict";
+
+    var $ = jQuery.noConflict(true);
 
     return declare("ModelReflectionSelector.widget.ModelReflectionSelector", [_WidgetBase, _TemplatedMixin], {
 
@@ -30,6 +33,8 @@ define([
         // Internal variables.
         _handles: null,
         _contextObj: null,
+        _entities: null,
+        _associations: null,
 
         constructor: function() {
             this._handles = [];
@@ -37,20 +42,38 @@ define([
 
         postCreate: function() {
             logger.debug(this.id + ".postCreate");
+            $(this.submitButton).on('click', function() {
+                console.log($('.mxreflectionselector').jstree().get_selected(true));
+            });
         },
 
         update: function(obj, callback) {
             mx.data.get({
                 xpath: "//MxModelReflection.MxObjectType",
-                callback: function(objs) {
+                // xpath: "//MxModelReflection.MxObjectReference",
+                callback: lang.hitch(this, function(objs) {
                     console.log("Received " + objs.length + " MxObjects");
-                    this._renderCheckboxes(objs);
-                }
+                    // this._rfxObjects = objs;
+                    // this._renderCheckboxes(objs);
+                    this._entities = objs; // set entities
+                    mx.data.get({
+                        // xpath: "//MxModelReflection.MxObjectType",
+                        xpath: "//MxModelReflection.MxObjectReference",
+                        callback: lang.hitch(this, function(objs) {
+                            console.log("Received " + objs.length + " MxObjects");
+                            // this._rfxObjects = objs;
+                            // this._renderCheckboxes(objs);
+                            this._associations = objs; // set associations...
+                            this._contextObj = obj;
+                            this._updateRendering(callback);
+                        })
+                    }, this);
+                })
             }, this);
+
             logger.debug(this.id + ".update");
 
-            this._contextObj = obj;
-            this._updateRendering(callback);
+
         },
 
         resize: function(box) {
@@ -61,20 +84,68 @@ define([
             logger.debug(this.id + ".uninitialize");
         },
 
-        _renderCheckboxes: function(objects) {
-            objects.forEach(lang.hitch(this, function(obj) {
-                this.domNode.innerHTML += '    ' + obj.get('Name');
-            }))
+        _getChildrenNodesForParent: function(parent) {
+            var ret = [];
+            // given a parent ID
+            var pid = parent.getGuid();
+            // find all associations whose parent or child is this parent ID
+            this._associations.forEach(function(a) {
+                if (a.get("MxModelReflection.MxObjectReference_MxObjectType_Parent")[0] === pid ||
+                    a.get("MxModelReflection.MxObjectReference_MxObjectType_Child")[0] === pid) {
+                    ret.push(a);
+                }
+            });
+            return ret.map(function(association) {
+                    return {
+                        text: association.get('Name'),
+                        type: 'assc'
+                    }
+                })
+                // .get("MxModelReflection.MxObjectReference_MxObjectType_Parent")
+                // .get("MxModelReflection.MxObjectReference_MxObjectType_Child")
+        },
+
+        _renderCheckboxes: function() {
+            var data = this._entities.map(lang.hitch(this, function(obj) {
+                return {
+                    text: obj.get('Name'),
+                    type: 'obj',
+                    // query to grab the attributes
+                    children: this._getChildrenNodesForParent(obj),
+                }
+            }));
+            $('.mxreflectionselector').jstree({
+                plugins: ['checkbox', 'wholerow', 'types', 'state'],
+                checkbox: {
+                    keep_selected_style: false,
+                    three_state: true
+                },
+                types: {
+                    "obj": {
+                        icon: "glyphicon glyphicon-th-large",
+                        valid_children: ['attr', 'assc']
+                    },
+                    "attr": {
+                        icon: "glyphicon glyphicon-list"
+                    },
+                    "assc": {
+                        icon: "glyphicon glyphicon-link"
+                    }
+                },
+                core: {
+                    themes: {
+                        variant: 'large',
+                        stripes: true
+                    },
+                    data: data
+                }
+            });
         },
 
         _updateRendering: function(callback) {
             logger.debug(this.id + "._updateRendering");
 
-            // if (this._contextObj !== null) {
-            //     dojoStyle.set(this.domNode, "display", "block");
-            // } else {
-            //     dojoStyle.set(this.domNode, "display", "none");
-            // }
+            this._renderCheckboxes();
 
             this._executeCallback(callback);
         },
